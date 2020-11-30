b0d91b952c75f65423030c02499e06046ef2ee5c
Olivia Toolson <livytoolson@gmail.com>
2020-11-30 12:23:31 -0800

    created update route

--- index.js
+++ index.js
@@ -36,7 +36,17 @@ const User = {
         return user
     },
     update(id, changes) {
-
+        const user = users.find(user => user.id === id)
+        if (!user) {
+            return null
+        } else {
+            const updatedUser = { id, ...changes }
+            users = users.map(u => {
+                if (u.id === id) return updatedUser
+                return u
+            })
+            return updatedUser
+        }
     }
 }
 
@@ -58,7 +68,7 @@ server.get('/api/users', (req, res) => {
     if (users) {
         res.status(200).json(users)
     } else {
-        res.status(500).json({ errorMessage: "There was an error while saving the user to the database"})
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database." })
     }
 })
 server.get('/api/users/:id', (req, res) => {
@@ -67,7 +77,7 @@ server.get('/api/users/:id', (req, res) => {
     if (user) {
         res.status(200).json(user)
     } else {
-        res.status(404).json({ errorMessage: "The user information could not be retrieved." })
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
     }
 })
 server.delete('/api/users/:id', (req, res) => {
@@ -76,15 +86,26 @@ server.delete('/api/users/:id', (req, res) => {
     if (deleted) {
         res.status(200).json(deleted)
     } else {
-        res.status(404).json({ errorMessage: "The user with the specified ID does not exist."})
+        res.status(500).json({ errorMessage: "The user could not be removed." })
     }
 })
 server.put('/api/users/:id', (req, res) => {
-
+    const changes = req.body
+    const { id } = req.body
+    const updatedUserFromClient = req.body
+    if (!updatedUserFromClient.name || !updatedUserFromClient.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    const updatedUser = User.update(id, changes)
+    if (updatedUser) {
+        res.status(200).json(updatedUser)
+    } else {
+        res.status(500).json({  errorMessage: "The user information could not be modified." })
+    }
 })
 
 server.use('*', (req, res) => {
-    res.status(404),json({ errorMessage: 'Not found.'})
+    res.status(404).json({ errorMessage: "The user with the specified ID does not exist." })
 })
 
 server.listen(5000, () => {
