26366d55029416801048a212ea65bb811def02dc
Olivia Toolson <livytoolson@gmail.com>
2020-11-30 11:54:26 -0800

    created get user by id route

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ const User = {
         }
         users.push(newUser)
         return newUser
+    },
+    getById(id) {
+        return users.find(user => user.id === id)
+    },
+    delete(id) {
+
+    },
+    update(id, changes) {
+
     }
 }
 
@@ -49,7 +58,19 @@ server.get('/api/users', (req, res) => {
     }
 })
 server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const user = User.getById(id)
+    if (user) {
+        res.status(200).json(user)
+    } else {
+        res.status(404).json({ errorMessage: "The user information could not be retrieved." })
+    }
+})
+server.delete('/api/users/:id', (req, res) => {
 
+})
+server.put('/api/users/:id', (req, res) => {
+    
 })
 
 server.use('*', (req, res) => {
